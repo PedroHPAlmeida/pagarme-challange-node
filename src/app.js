3ea0f55cef1a13e8edc060a5523ae98ba3fac7fc
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-11 22:09:46 -0300

    feat: cria setup de conexão com o MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import db from "./config/db.js";
+
+db.on("error", console.log.bind(console, "Connection error"));
+db.once("open", () => {
+	console.log("The connection to the database was successful");
+});
 
 const app = express();
-
 
 export default app;
