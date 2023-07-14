b2bdd3d735f7e7f1749017263073d186fe3edaff
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-14 13:53:15 -0300

    fix: cria instância do modelo antes de salvar no banco

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -5,7 +5,7 @@ class TransactionService {
 	static async create(transaction) {
 		const lastFourNumbersOfCard = transaction.cardNumber.substring(transaction.cardNumber.length - 4); 
 		transaction.cardNumber = lastFourNumbersOfCard;
-		return await transactions.create(transaction);
+		return await transactions.create(new transactions(transaction));
 	}
 
 	static async findAll() {
