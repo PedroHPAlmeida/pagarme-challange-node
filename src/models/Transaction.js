cf66c5740a011d49982b7918387a1c0c5865b440
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-14 14:17:47 -0300

    feat: adiciona timestamps ao model Transaction

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -13,7 +13,7 @@ const transactionSchema = new mongoose.Schema({
 	cardholder: { type: String, required: [true, "cardholder is a required field"] },
 	cardExpirationDate: { type: Date, required: [true, "cardExpirationDate is a required field"] },
 	cardCvv: { type: Number, required: [true, "cardCvv is a required field"] }
-});
+}, { timestamps: true });
 
 const Transaction = mongoose.model("transactions", transactionSchema);
 
