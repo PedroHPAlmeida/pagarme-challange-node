b498b897eb28f730ea7efc9c9368afaeda0ea282
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-17 08:14:47 -0300

    refact: remove validação duplicada

--- src/services/PayableService.js
+++ src/services/PayableService.js
@@ -1,5 +1,4 @@
 import { payables } from "../models/index.js";
-import InvalidPayableStatusError from "../errors/InvalidPayableStatusError.js";
 import PaymentMethodFactory from "./PaymentMethodFactory.js"; 
 
 class PayableService {
@@ -17,10 +16,6 @@ class PayableService {
 	}
 
 	static async findPayablesByStatus(status) {
-		const possibleStatus = ["paid", "waiting_funds"];
-		if (!possibleStatus.includes(status)) {
-			throw new InvalidPayableStatusError(`payable status ${status} is not valid`);
-		}
 		return await payables.find({ status: status });
 	}
 
