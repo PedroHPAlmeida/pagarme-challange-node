d4b28ef9aaa1236c44a012e02a3d1fb8a32c4158
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-17 08:18:58 -0300

    style: remove comentário

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -6,7 +6,6 @@ class TransactionService {
 	static async create(transaction) {
 		const lastFourNumbersOfCard = transaction.cardNumber.substring(transaction.cardNumber.length - 4); 
 		transaction.cardNumber = lastFourNumbersOfCard;
-		// TODO - validar o payable e a transaction antes de tentar salvar, para que uma não seja salva sem a outra
 		const savedTransaction = await transactions.create(transactions(transaction));
 		await PayableService.create(savedTransaction);
 		return savedTransaction;
